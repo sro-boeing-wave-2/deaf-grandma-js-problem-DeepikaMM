542308e64340c19d0dfcc26ca517a8adcf792879
Deepika Mohan <deepikamm12@gmail.com>
2018-07-10 10:48:37 +0530

    updated

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
  * @fileOverview Contains logic of Deaf Grandma
  */
 const isUppercase = require('./isUpperCase');
-const getRandomValue = require('./getRandomValue');
+/**
+ * const getRandomValue = require('./getRandomValue');
+ */
+
 const { prompt, print } = require('./prompt-print');
 
 /**
@@ -13,3 +16,25 @@ const { prompt, print } = require('./prompt-print');
  * 1. prompt function to get input from the user
  * 2. print function to print text on to the console
  */
+let count = 0;
+const flag = true;
+print('What do you want to say to Grandma!!');
+let message = prompt('send your message');
+while (flag === true) {
+  if (isUppercase(message)) {
+    if (message === 'BYE') {
+      count += 1;
+      if (count > 2) {
+        break;
+      }
+    }
+    print('NO, NOT SINCE 1930');
+    /**
+     * print('NO, NOT SINCE '.concat(getRandomValue(1932, 1950)));
+     */
+  } else {
+    count = 0;
+    print('HUH?! SPEAK UP, SONNY!');
+  }
+  message = prompt('send your message');
+}
